4088c84697b10d4e0b28927198ecc1775179cbb0
the only one <106877000+yousefessam44034@users.noreply.github.com>
2023-11-29 23:57:31 +0200

    Update mydatabase.sql

--- mydatabase.sql
+++ mydatabase.sql
@@ -1,11 +1,1 @@
-CREATE DATABASE mydatabase;
 
--- Use the database
-USE mydatabase;
-
--- Create the users table
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
